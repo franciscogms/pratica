150720b80735f00875d2d4bd19af4b4602d4795a
franciscogms <francisco1045gomesa@gmail.com>
2025-05-19 13:16:29 -0300

    login
    meu login

--- process_register.php
+++ process_register.php
@@ -1,34 +1,32 @@
 <?php 
-// Inclui o arquivo de conexão com o banco de dados
+
 require 'config/db.php';
 
-// verifica se o formulário foi enviado utilizando o método POST
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+// Verifica se o formulário foi enviado utilizando o método POST
+if ($_SERVER ["REQUEST_METHOD"] == "PODST") {
 
-    // Pega os dados do formulário enviados pelo método POST
-    $nome = $_POST['nome'] // Nome digitado pelo usuário
-    $email = $_POST['email'] // Email digitado pelo usuário
+//pega os dados do formulário enviados pelo método POST
+$nome = $_POST [ 'nome'];     //Nome digitado pelo usuário 
+$email = $_POST['email'];     //Email digitado pelo usuário
 
-    // Criptografa a senha antes de salvar no banco de dados
-    $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT); 
-    
-    // prepara a consulta a query SQL com parametros (?) para evitar SQL Injection
-    $stmt = $conn->prepare(query:"INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)");
+//Criptografia a senha antes de salvar no banco de dados
+// password_hash() é uma função segura que utiliza o algoritimo bcrypt por padrão 
+$senha = password_hash($_POST ['senha'], PASSWORD_DEFAULT);
 
-    // substitui os parâmetros da query pelos valores reais da forma segura
-    // "SSS" significa que estamos passando 3 strings 
+//Prepara a query SQL com paramêtros (?) para evitar SQL Injection
+$stmt = $conn->prepare("INSERT INTO usuário (nome, email, senha) VALUES (?,?,?)");
 
+//substitui os parâmetros da query pelos valores reais de forma segura
+//"sss" significa que estamos passando 3 strings ( s = string)
+$stmt ->bind_param ("sss" , $nome, $email, $senha);
+
+// Executa a query SQL no banco de dados
+if ($stmt->execute()) {
+// Se  o cadastro for feito com sucesso, redireciona para a tela de login
+// Envia o parâmetro "sucesso=1" na URL para mostrar mensagem no login
+header("Location: login.php?sucesso=1");
+exit; // Finaliza o script imediatamente após
 }
 
-
-
-
-
-
-
-
-
-
-
-
+}
 ?>
